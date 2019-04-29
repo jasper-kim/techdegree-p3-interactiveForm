474ed5659d8cb2be6d6595c84d8eecf6238fdd6a
Jasper Kim <yjikim0219@gmail.com>
2019-04-29 02:39:38 -0700

    Update Real Time validation-2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+
+/**
+ * 
+ * Basic Info Section
+ *  
+ */
+
 //focus on the first text field
 $('input[type="text"]:first').focus();
 
@@ -15,12 +22,24 @@ $('#title').on('change', event => {
     }
 });
 
+/**
+ * 
+ * T-Shirt Info Section
+ *  
+ */
+
+//hide the T-shirt 'color' menu when the page is loaded
+$('#colors-js-puns').hide();
+
 //add an event listener to T-Shirt Info section
 $('#design').on('change', event => {
     //get the value of selected design
     const $theme = $('#design option:selected').val();
     //assign option elements to a variable
     const $colors = $('#color option');
+
+    //show the T-shirt 'color' menu when an user selects a design
+    $('#colors-js-puns').show();
 
     //reset the attributes of color options
     $colors.each(function(){
@@ -43,9 +62,15 @@ $('#design').on('change', event => {
 
         $colors.eq(3).attr('selected', true);
     } else{
-        $colors.eq(0).attr('selected', true);
+        $('#colors-js-puns').hide();
     }
 });
+
+/**
+ * 
+ * Register for Activities Info Section
+ *  
+ */
 
 //add an event listener to Register for Activities Info section
 $('.activities').on('click', (e) => {
@@ -126,6 +151,12 @@ $('.activities').on('click', (e) => {
     }
 });
 
+/**
+ * 
+ * Payment Info Section
+ *  
+ */
+
 //add the hidden attribute to "Select Payment Method" option 
 //so that an user should select one of payment options
 $('#payment option:first').attr('hidden', true);
@@ -159,15 +190,29 @@ $('#payment').on('change', (e) => {
     }
 });
 
+/**
+ * 
+ * Form validation
+ *  
+ */
+
+ //pick elements to check validation
+const $nameInput = $('#name');
+const $mailInput = $('#mail');
+const $actInput = $('.activities legend');
+const $cardNumInput = $('#cc-num');
+const $zipCodeInput = $('#zip');
+const $cvvInput = $('#cvv');
+
 //append error messages to each input section
-$('<span>Please, enter your name.</span>').insertAfter('#name');
-$('<span>Please, enter a valid email. ex) example@domain.com</span>').insertAfter('#mail');
-$('<span>Please, select at least one activity.</span>').insertAfter('.activities legend');
+$('<span>Please enter your name.</span>').insertAfter('#name');
+$('<span>Please enter a valid email. ex) example@domain.com</span>').insertAfter('#mail');
+$('<span>Please select at least one activity.</span>').insertAfter('.activities legend');
 $('<span>Enter between 13 and 16 digit number.</span>').insertAfter('#cc-num');
 $('<span>Enter 5-digit number.</span>').insertAfter('#zip');
 $('<span>Enter 3-digit number.</span>').insertAfter('#cvv');
 
-//make functions to check each 
+//make functions to check if the each section is acceptable
 function isValidUsername(username) {
     return /^[a-zA-Z_]+$/.test(username);
 }
@@ -175,59 +220,68 @@ function isValidEmail(email) {
     return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
 }
 function isValidCheckbox() {
+    console.log($('input[type="checkbox"]:checked').length);
     return $('input[type="checkbox"]:checked').length > 0;
 }
 function isValidCardNum(number) {
-    return /\d{13,16}/.test(number);
+    return /^\d{13,16}$/.test(number);
 }
 function isValidZipCode(number) {
-    return /\d{5}/.test(number);
+    return /^\d{5}$/.test(number);
 }
 function isValidCVV(number) {
-    return /\d{3}/.test(number);
+    return /^\d{3}$/.test(number);
+}
+
+//if the validator argument return false, display error message
+//if not, let the page reloaded
+function showOrHideError(event, validator, selector) {
+    if(!validator(selector.val())) {
+        selector.prev().css('color', 'red');
+        selector.css('border-color','red');
+        selector.next().css('display','inherit');
+        event.preventDefault();
+    } else {
+        selector.prev().css('color', 'initial');
+        selector.css('border-color','#c1deeb');
+        selector.next().css('display','none');
+    }
 }
 
 //when the register button is clicked, check validation of all the input section
-//if all the input valid, let the page reload
-//if not, prevent submitting the form and turn the border of input section red, also display error message
-$('form').on('submit', (e) => {
-     const $nameInput = $('#name');
-     const $mailInput = $('#mail');
-     const $cardNumInput = $('#cc-num');
-     const $zipCodeInput = $('#zip');
-     const $cvvInput = $('#cvv');
-
-    function showOrHideError(validator, selector) {
-        if(!validator(selector.val())) {
-            selector.prev().css('color', 'red');
-            selector.css('border-color','red');
-            selector.next().css('display','inherit');
-            e.preventDefault();
-        } else {
-            selector.prev().css('color', 'initial');
-            selector.css('border-color','#c1deeb');
-            selector.next().css('display','none');
-        }
-    }
-     
-    showOrHideError(isValidUsername, $nameInput);
-    showOrHideError(isValidEmail, $mailInput);
+$('form').on('submit', (event) => {
+    showOrHideError(event, isValidUsername, $nameInput);
+    showOrHideError(event, isValidEmail, $mailInput);
 
     if(!isValidCheckbox()) {
-        $('.activities legend').css('color', 'red');
-        $('.activities legend').next().css('display','inherit');
-        e.preventDefault();
+        $actInput.css('color', 'red');
+        $actInput.next().css('display','inherit');
+        event.preventDefault();
     } else {
-        $('.activities legend').css('color', '#184f68');
-        $('.activities legend').next().css('display','none');
+        $actInput.css('color', '#184f68');
+        $actInput.next().css('display','none');
     }
     
     if($('#payment option:selected').val() === 'credit card') {
-        showOrHideError(isValidCardNum, $cardNumInput);
-        showOrHideError(isValidZipCode, $zipCodeInput);
-        showOrHideError(isValidCVV, $cvvInput);
+        showOrHideError(event, isValidCardNum, $cardNumInput);
+        showOrHideError(event, isValidZipCode, $zipCodeInput);
+        showOrHideError(event, isValidCVV, $cvvInput);
     }
 });
 
-
-
+//add a real time event listener to the each section for validation check
+$nameInput.on('input', (event) => {
+    showOrHideError(event, isValidUsername, $nameInput);
+});
+$mailInput.on('input', (event) => {
+    showOrHideError(event, isValidEmail, $mailInput);
+});
+$cardNumInput.on('input', (event) => {
+    showOrHideError(event, isValidCardNum, $cardNumInput);
+});
+$zipCodeInput.on('input', (event) => {
+    showOrHideError(event, isValidZipCode, $zipCodeInput);
+});
+$cvvInput.on('input', (event) => {
+    showOrHideError(event, isValidCVV, $cvvInput);
+});
